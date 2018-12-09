b686e98b659ffdf620adeda52f39f85edd03bd28
Debashish Deka <dekadebashish3@gmail.com>
2018-12-09 17:31:23 +0530

    c++ tools added

---
+++ setup_ubuntu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt install g++
+sudo apt install valgrind
+sudo apt install htop
+sudo apt install clang
+sudo apt install cmake
+sudo apt install terminator
+sudo apt-get install silversearcher-ag
